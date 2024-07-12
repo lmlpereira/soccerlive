f6f2b9eadc398a9d86ae6240c8718adc7e45e13b
Luis Pereira <luispereira21@live.com.pt>
2024-07-12 11:24:37 +0100

    new file:   lib/api_manager.dart 	new file:   lib/goalstat.dart 	modified:   lib/main.dart 	new file:   lib/matchtile.dart 	new file:   lib/pagebody.dart 	new file:   lib/soccermodel.dart 	new file:   lib/teamstat.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

---
+++ lib/api_manager.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:soccerlive/soccermodel.dart';
+
+class SoccerApi{
+
+  final String apiUrl = "https://v3.football.api-sports.io/fixtures?live=all";
+
+  static const api_Key = "2c3fa9f813361a756b8021d88c3f60fe";
+
+  static const headers = { 'x-rapidapi-key': '$api_Key','x-rapidapi-host': "v3.football.api-sports.io"};
+
+
+  Future<List<SoccerMatch>?> getAllMatches() async {
+
+    Uri uri = Uri.parse(apiUrl);
+
+    Response res = await get(uri, headers: headers);
+
+    var body;
+    debugPrint("API SERVICE: ${res.statusCode}");
+
+
+    if(res.statusCode == 200){
+      body = jsonDecode(res.body);
+
+      List<dynamic> matchesList = body['response'];
+
+      debugPrint("API SERVICE: ${body}");
+
+      try{
+        List<SoccerMatch> matches = matchesList.map((dynamic item) => SoccerMatch.fromJson(item)).toList();
+            return matches;
+
+      }catch(ex){
+        debugPrint("API Error: ${ex.toString()}");
+
+      }
+    
+    }
+
+    
+
+
+  }
+
+
+
+}
---
+++ lib/matchtile.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+import 'package:soccerlive/soccermodel.dart';
+
+Widget matchTile(SoccerMatch match){
+  
+  return Container(
+    margin: EdgeInsets.symmetric(vertical: 12.0),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(child: Text(match.home.name, textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 16.0,),)),
+        Image.network(match.home.logoUrl, width: 36.0,),
+        Expanded(child: Text("${match.goal.home} - ${match.goal.away}", textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 16.0,),)),
+        Image.network(match.away.logoUrl, width: 36.0,),
+        Expanded(child: Text(match.away.name, textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 16.0,),)),
+
+      ],
+    ),
+  );
+}
